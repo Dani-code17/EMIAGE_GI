6eeed60ea1a835de6284c457542cfc9239936522
unknown <yeopauldaniel@gmail.com>
2025-09-13 17:41:45 +0000

    Move sitemap.xml to root URLconf

--- emiage_web/urls.py
+++ emiage_web/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', core_views.sitemap_xml, name='sitemap_xml'),
     path('', include('core.urls')),  # Inclure les URLs de l'application core
 ]
 
