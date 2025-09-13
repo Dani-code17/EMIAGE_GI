b82fc5773591929a5d650cf26fb7fed8d6e62419
unknown <yeopauldaniel@gmail.com>
2025-09-13 15:47:00 +0000

    Search additional templates dir (BASE_DIR/templates) to avoid TemplateDoesNotExist on deploy

--- emiage_web/settings.py
+++ emiage_web/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'emiage_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'core' / 'templates'],
+    'DIRS': [BASE_DIR / 'core' / 'templates', BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
